31400c62e952fa056ab2c3aa6e6606d3b42bfa86
abdul ajees <abdulajee2609@gmail.com>
2025-07-10 11:28:01 +0530

    update

--- db_config.py
+++ db_config.py
@@ -1,11 +1,13 @@
- 
 import mysql.connector
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load values from .env
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("MYSQL_HOST"),
-        user=os.getenv("MYSQL_USER" ),
-        password=os.getenv("MYSQL_PASSWORD" ),
-        database=os.getenv("MYSQL_DB" )
+        host=os.getenv("MYSQL_HOST", "localhost"),
+        user=os.getenv("MYSQL_USER", "root"),
+        password=os.getenv("MYSQL_PASSWORD", ""),
+        database=os.getenv("MYSQL_DB", "pixdot")
     )
