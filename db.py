f6cfb36f7c3f26e536a123c3c7f6b406077fdfdb
abdul ajees <abdulajee2609@gmail.com>
2025-07-10 12:58:07 +0530

    update

--- db.py
+++ db.py
@@ -1,14 +1,15 @@
-import os
 import psycopg2
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
-        host=os.getenv("PGHOST"),
-        database=os.getenv("PGDATABASE"),
-        user=os.getenv("PGUSER"),
-        password=os.getenv("PGPASSWORD"),
-        port=os.getenv("PGPORT", 5432)
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        sslmode="require"
     )
